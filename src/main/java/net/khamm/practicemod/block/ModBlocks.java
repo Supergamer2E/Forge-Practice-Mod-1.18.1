782a07d6eb33bce883ddc0bc8bfaa9e9598ca97b
Supergamer2E <khamm0427@gmail.com>
2023-05-15 11:40:45 -0400

    Added Blocks and Items

--- src/main/java/net/khamm/practicemod/block/ModBlocks.java
+++ src/main/java/net/khamm/practicemod/block/ModBlocks.java
@@ -20,23 +20,27 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, PracticeMod.MOD_ID);
 
-    public static final RegistryObject<Block> CITRINE_BLOCK = registerBlock("citrine_block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-            .strength(9f).requiresCorrectToolForDrops()), ModCreativeModeTab.PRACTICE_TAB);
+    public static final RegistryObject<Block> CITRINE_BLOCK = registerBlock("citrine_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
+                    .strength(9f).requiresCorrectToolForDrops()), ModCreativeModeTab.PRACTICE_TAB);
 
-    public static final RegistryObject<Block> RAW_CITRINE_BLOCK = registerBlock("raw_citrine_block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-            .strength(9f).requiresCorrectToolForDrops()), ModCreativeModeTab.PRACTICE_TAB);
+    public static final RegistryObject<Block> RAW_CITRINE_BLOCK = registerBlock("raw_citrine_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
+                    .strength(7f).requiresCorrectToolForDrops()), ModCreativeModeTab.PRACTICE_TAB);
 
-    public static final RegistryObject<Block> CITRINE_ORE = registerBlock("citrine_ore", () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-            .strength(9f).requiresCorrectToolForDrops()), ModCreativeModeTab.PRACTICE_TAB);
+    public static final RegistryObject<Block> CITRINE_ORE = registerBlock("citrine_ore",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(5f).requiresCorrectToolForDrops()), ModCreativeModeTab.PRACTICE_TAB);
 
-    public static final RegistryObject<Block> DEEPSLATE_CITRINE_ORE = registerBlock("deepslate_citrine_ore", () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-            .strength(9f).requiresCorrectToolForDrops()), ModCreativeModeTab.PRACTICE_TAB);
-
-    public static final RegistryObject<Block> ENDSTONE_CITRINE_ORE = registerBlock("endstone_citrine_ore", () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-            .strength(9f).requiresCorrectToolForDrops()), ModCreativeModeTab.PRACTICE_TAB);
-
-    public static final RegistryObject<Block> NETHERRACK_CITRINE_ORE = registerBlock("netherrack_citrine_ore", () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-            .strength(9f).requiresCorrectToolForDrops()), ModCreativeModeTab.PRACTICE_TAB);
+    public static final RegistryObject<Block> DEEPSLATE_CITRINE_ORE = registerBlock("deepslate_citrine_ore",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(7f).requiresCorrectToolForDrops()), ModCreativeModeTab.PRACTICE_TAB);
+    public static final RegistryObject<Block> NETHERRACK_CITRINE_ORE = registerBlock("netherrack_citrine_ore",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(5f).requiresCorrectToolForDrops()), ModCreativeModeTab.PRACTICE_TAB);
+    public static final RegistryObject<Block> ENDSTONE_CITRINE_ORE = registerBlock("endstone_citrine_ore",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(5f).requiresCorrectToolForDrops()), ModCreativeModeTab.PRACTICE_TAB);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
@@ -44,8 +48,10 @@ public class ModBlocks {
         return toReturn;
     }
 
-    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
-        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+                                                                            CreativeModeTab tab) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(tab)));
     }
 
     public static void register(IEventBus eventBus) {
